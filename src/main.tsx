de87273e7c4222f718cbd9f1be6134abc0bce7cf
Markhadi <markhadi@protonmail.com>
2024-11-23 06:06:34 +0800

    feat(app): integrate UserProvider and router setup
    - Add UserProvider wrapper
    - Setup router provider
    - Add toast notifications
    - Setup proper provider order

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import { router } from './routes';
+import { router } from '@/routes';
 import { Toaster } from '@/components/ui/toaster';
+import { UserProvider } from '@/contexts/UserContext';
 import '@/styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-    <Toaster />
+    <UserProvider>
+      <RouterProvider router={router} />
+      <Toaster />
+    </UserProvider>
   </React.StrictMode>
 );
